10d88128c306357c48527ac3b3c327d98c71ea10
Matt Crook <mattlcrook@icloud.com>
2020-04-23 20:21:38 +1200

    sample ssh client

--- Workers/RemoteMediaWorker.cs
+++ Workers/RemoteMediaWorker.cs
@@ -3,6 +3,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Renci.SshNet;
 
 namespace Sshanty.Workers
 {
@@ -11,10 +13,12 @@ namespace Sshanty.Workers
         public const int CYCLE_PERIOD = 1000 * 60 * 15; // 15 minutes
 
         private readonly ILogger<RemoteMediaWorker> _logger;
+        private readonly IConfiguration _config;
 
-        public RemoteMediaWorker(ILogger<RemoteMediaWorker> logger)
+        public RemoteMediaWorker(ILogger<RemoteMediaWorker> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -23,6 +27,18 @@ namespace Sshanty.Workers
             {
                 _logger.LogDebug("Began executing task");
                 var sw = Stopwatch.StartNew();
+
+                var connectionInfo = new ConnectionInfo(
+                    _config["Remote:HostName"], _config["Remote:UserName"],
+                    new PrivateKeyAuthenticationMethod(
+                        _config["Remote:UserName"], new PrivateKeyFile(_config["Remote:PrivateKeyFile"])));
+
+                using (var client = new SshClient(connectionInfo))
+                {
+                    client.Connect();
+                    var cmd = client.RunCommand("ls -la");
+                    _logger.LogInformation(cmd.Result);
+                }
 
                 sw.Stop();
                 _logger.LogDebug("Finished executing task in {0}", sw.Elapsed);
